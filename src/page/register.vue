b270667fcaeb96551e13545a3dec2b1cd4ad45dd
lifanjie <610248668@qq.com>
2017-11-16 22:04:22 +0800

    20171116

--- src/page/register.vue
+++ src/page/register.vue
@@ -1,47 +1,55 @@
 <template>
-<div>
-  <div class="page-field">
-    <myHeader></myHeader>
-  </div>  
+  <div>
+    <div class="page-field">
+      <myHeader></myHeader>
+    </div>  
 
-  <div class="page-tab-container">
-    <mt-tab-container v-model="active">
-    
-    <mt-tab-container-item id="tab-container1">
-      <div class="page-part">
-        <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
-        <mt-field label="验证码" placeholder="请输入验证码" v-model="captcha">
-          <div class="cond_num"><span class="valiCode">免费获取</span></div>
-        </mt-field>
-      </div>
-      <div class="page-button">
-        <mt-button type="primary" size="large">验证</mt-button>
-      </div>
-    </mt-tab-container-item>
-    
-    <mt-tab-container-item id="tab-container2">
+    <div class="page-tab-container">
+      <mt-tab-container v-model="active">
+      
+      <mt-tab-container-item id="tab-container1">
+        <div class="page-part">
+          <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
+          <mt-field label="验证码" placeholder="请输入验证码" v-model="captcha">
+            <div class="cond_num"><span class="valiCode">免费获取</span></div>
+          </mt-field>
+        </div>
+        <div class="page-button">
+          <mt-button  type="primary" @click.native="validate()" size="large">验证</mt-button>
+        </div>
+      </mt-tab-container-item>
+      
+      <mt-tab-container-item id="tab-container2">
 
-    </mt-tab-container-item> 
+      </mt-tab-container-item> 
 
-    </mt-tab-container>
+      </mt-tab-container>
+    </div>
+
   </div>
-
-</div>
 </template>
 
 <script>
 import Vue from 'vue'
 import myHeader from '../components/header'
-import { Field } from 'mint-ui'
+import { Field, Toast, Button } from 'mint-ui'
 
-Vue.component(Field.name, Field)
+Vue.component(Field)
+Vue.component(Button)
+Vue.component(Toast)
 
 export default {
   components: {myHeader},
-  name: 'page-tab-container',
   data () {
     return {
-      active: 'tab-container1'
+      active: 'tab-container1',
+      captcha: '',
+      phone: ''
+    }
+  },
+  methods: {
+    validate: function () {
+      Toast(this.active)
     }
   }
 }
