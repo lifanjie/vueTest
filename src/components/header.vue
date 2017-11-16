b4a68270c6d3ce7e6bedfdc002a87dd3ea507d79
lifanjie <610248668@qq.com>
2017-11-16 20:04:03 +0800

    20171116update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,5 @@
 <template>
-<<<<<<< HEAD
-
-  <mt-header fixed title="固定在顶部"></mt-header>
-
-  
-=======
   <mt-header fixed title="会员注册"></mt-header>
->>>>>>> 5b5a64fa56d1c98f83b8c2d328c1de7578906e55
 </template>
 
 
