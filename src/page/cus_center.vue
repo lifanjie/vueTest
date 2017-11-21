08bba7147e233712f4b4f8fac972da745c140a30
lifanjie <610248668@qq.com>
2017-11-21 17:31:29 +0800

    201711121company

--- src/page/cus_center.vue
+++ src/page/cus_center.vue
@@ -114,7 +114,7 @@ export default {
 </script>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../style/scss/_cus_center";
 </style>
 
---
+++ src/page/exchange_detail.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="head">
+	    <div class="head_left">
+		    <p class="giftName_p"><span id="giftName">{{giftName}}</span></p>
+		    <p class="price_p">原价 <span id="price">{{price}}</span></p>
+	    </div>
+	    <div class="head_right">
+		    <p><span id="spendPoints">{{spendPoints}}</span> 积分</p>
+	    </div>
+    </div>
+    
+    <div class="list"></div>
+    <div class="free"><p>详情介绍</p></div>
+
+
+    <section class="wrap">
+          <div class="fgray" id="recommend"></div>
+    </section>
+	
+    <div class="add_new_address">
+      <a class="exchange"><div class="add_new_address_body">兑换</div></a> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: '1',
+      id: '',
+      orgId: '',
+      openId: '',
+      giftName: '',
+      price: '',
+      spendPoints: ''
+    }
+  },
+  created: function () {
+    this.id = this.$route.query.id
+    this.orgId = this.$route.query.orgId
+    this.openId = this.$route.query.openId
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<<style lang="sass" scoped>
+@import "../style/scss/_exchange_detail";
+</style>
+
+
