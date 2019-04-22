aa39a61061ed351c1512aaaaf50ad148ca9ad006
lifanjie <610248668@qq.com>
2019-04-23 00:17:27 +0800

    20190423

--- config/prod.env.js
+++ config/prod.env.js
@@ -8,7 +8,7 @@
 
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: '"https://ht.zuanno.cn/wechat"',
-  ROOT: '"https://ht.zuanno.cn"',
-  WS: '"wss://ht.zuanno.cn"',
+  API_BASE_URL: '"https://ht.zhuxiaobao.net/wechat"',
+  ROOT: '"https://ht.zhuxiaobao.net"',
+  WS: '"wss://ht.zhuxiaobao.net"',
 }
